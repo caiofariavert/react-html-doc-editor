f117211fd181a05531036ef2889c420165987fc7
Caio Faria <caio.faria@vert-capital.com>
2026-02-24 11:14:16 -0300

    fix: correct build structure and exports

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+export { default as Editor } from './components/Editor.js';
+export { default as Toolbar } from './components/Toolbar.js';
+export { default as TableButton } from './components/TableButton.js';
+export { default as DocxImport } from './components/DocxImport.js';
+export * from './utils/htmlCompiler.js';
+export * from './utils/datasetUtils.js';
